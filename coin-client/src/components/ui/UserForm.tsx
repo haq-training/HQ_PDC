c8b9a3b3bd85cfb2e4588e33c6f6103f76d2508b
hoaiphuong123 <hp34nam@gmail.com>
2023-07-04 11:53:46 +0700

    fix sua ten user

--- coin-client/src/components/ui/UserForm.tsx
+++ coin-client/src/components/ui/UserForm.tsx
@@ -1,13 +1,12 @@
 import TextField from '@material-ui/core/TextField';
 import React, { useState } from 'react';
 import Card from '@material-ui/core/Card';
-import jwt from 'jsonwebtoken';
 import axios from 'axios';
+import jwt from 'jsonwebtoken';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
 import CardHeader from '@material-ui/core/CardHeader';
-import Button from '@/components/ui/button';
 import Image from '@/components/ui/image';
+import Button from '@/components/ui/button';
 
 type ItemType = {
     userName: string;
@@ -20,8 +19,32 @@ type CardProps = {
 };
 
 export default function UserForm({ item }: CardProps) {
-    const data = React.useMemo(() => item, [item]);
-    const { userName = '', userAvarta, userCover } = data;
+    const { userName = '', userAvarta, userCover } = item;
+    const [newUserName, setNewUserName] = useState(userName);
+    const handleUserNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewUserName(e.target.value);
+    };
+
+    const handleSaveUserName = async () => {
+        try {
+            const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
+            const decodedToken: any = jwt.decode(token);
+            const userId = decodedToken?.idUser;
+            await axios.put(
+                `http://localhost:4003/users/update-name/${userId}`,
+                { userName: newUserName },
+                {
+                    headers: {
+                        token: `Bearer ${token}`
+                    },
+                    method: 'PUT'
+                }
+            );
+            console.log('UserName updated successfully!');
+        } catch (error) {
+            console.error('Failed to update UserName:', error);
+        }
+    };
 
     return (
         <Card className="mt-10" style={{ width: '500px' }}>
@@ -31,36 +54,31 @@ export default function UserForm({ item }: CardProps) {
                     <div className="flex flex-row">
                         <div className="flex flex-col items-center ml-16">
                             {userAvarta && (
-                                        <Image
-                                            src={userAvarta}
-                                            placeholder="empty"
-                                            width={60}
-                                            priority
-                                            quality={100}
-                                            height={60}
-                                            alt={userName}
-                                        />
+                                <Image
+                                    src={userAvarta}
+                                    placeholder="empty"
+                                    width={60}
+                                    priority
+                                    quality={100}
+                                    height={60}
+                                    alt={userName}
+                                />
                             )}
-                            <label>
-                                Ảnh đại diện
-                            </label>
+                            <label>Ảnh đại diện</label>
                         </div>
                         <div className="flex flex-col items-center ml-16">
                             {userCover && (
-
-                                        <Image
-                                            src={userCover}
-                                            placeholder="empty"
-                                            width={60}
-                                            priority
-                                            quality={100}
-                                            height={60}
-                                            alt={userName}
-                                        />
+                                <Image
+                                    src={userCover}
+                                    placeholder="empty"
+                                    width={60}
+                                    priority
+                                    quality={100}
+                                    height={60}
+                                    alt={userName}
+                                />
                             )}
-                            <label>
-                                Ảnh bìa
-                            </label>
+                            <label>Ảnh bìa</label>
                         </div>
                     </div>
                 </div>
@@ -71,8 +89,10 @@ export default function UserForm({ item }: CardProps) {
                         label="Tên người dùng"
                         placeholder="Tên người dùng"
                         margin="normal"
-                        value={userName}
+                        value={newUserName}
+                        onChange={handleUserNameChange}
                     />
+                    <Button onClick={handleSaveUserName}>Lưu</Button>
                 </div>
             </CardContent>
         </Card>
